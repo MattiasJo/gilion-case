e2b677311b4d77f12dd92c6e18c69d38dcc0a93a
Mattias Johansson <mattiasj81@gmail.com>
2024-09-24 13:31:47 +0200

    Responsivity fixes

--- src/Containers/ChartContainer.tsx
+++ src/Containers/ChartContainer.tsx
@@ -17,23 +17,23 @@ export const ChartContainer = () => {
     <CircularProgress />
   ) : (
     <Grid container spacing={2} padding={2} maxWidth="1280px">
-      <Grid size={{ xs: 12, sm: 8 }}>
+      <Grid size={{ xs: 12, md: 8 }} order={1}>
         <LineChart
           dataByYear={dataByYear}
           marketSpendByYear={marketSpendByYear}
         />
       </Grid>
-      <Grid size={{ xs: 12, sm: 4 }}>
+      <Grid size={{ xs: 12, sm: 6, md: 4 }} order={{ xs: 3, md: 2 }}>
         <PieChart
           countryCodes={countryCodes}
           dataByCountry={dataByCountry}
           type="Marketing Spend"
         />
       </Grid>
-      <Grid size={{ xs: 12, sm: 8 }}>
+      <Grid size={{ xs: 12, md: 8 }} order={{ xs: 2, md: 3 }}>
         <BarChart countryCodes={countryCodes} dataByCountry={dataByCountry} />
       </Grid>
-      <Grid size={{ xs: 12, sm: 4 }}>
+      <Grid size={{ xs: 12, sm: 6, md: 4 }} order={4}>
         <PieChart
           countryCodes={countryCodes}
           dataByCountry={dataByCountry}
